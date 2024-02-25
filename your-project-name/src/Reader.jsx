5ab125b8c6d7fb57313577ed37769de0e767be21
Vedita Deshpande <42276546+veditadeshpande@users.noreply.github.com>
2024-02-24 18:31:22 -0800

    created main pages

--- your-project-name/src/Reader.jsx
+++ your-project-name/src/Reader.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function Reader() {
   const [count, setCount] = useState(0)
 
   return (
@@ -25,11 +25,8 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default Reader
