efd178812b10cd6a7acf9cfa8e7b02aeb6a3e05e
Vedita Deshpande <42276546+veditadeshpande@users.noreply.github.com>
2024-02-25 00:53:46 -0800

    added changes for reader page

--- your-project-name/src/Reader.jsx
+++ your-project-name/src/Reader.jsx
@@ -1,6 +1,5 @@
 //import { useState } from 'react'
 import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
@@ -35,20 +34,25 @@ function Reader() {
 
   return (
     <>
-    <div>
-      <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-    </div>
     <h1>Sample news name</h1>
-    <div className="card">
-      <h3>Sample title </h3>
-      <h4>
 
+  <div className='mainNewsSection'>
+
+  <div className="newsText">
+      <h3>Sample Source for the news/ Author ?? </h3>
+      <div className="newsContent">
+      <h4>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit. Curabitur sapien tellus, consequat sit amet, tempor quis, porta eget, orci. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc.
     </h4>
-
+      </div>
     <div className='customization'>
+    <Box sx={{ width: 300 }}>
+      <Slider
+        aria-label="Neutrality"
+        defaultValue={30}
+        color="secondary"
+      />
+    </Box>
     <Box
       sx={{
         display: 'flex',
@@ -63,16 +67,11 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendi
         {buttons}
       </ButtonGroup>
     </Box>
-    <Box sx={{ width: 300 }}>
-      <Slider
-        aria-label="Neutrality"
-        defaultValue={30}
-        color="secondary"
-      />
-    </Box>
     </div>
  
     </div>
+  </div>
+
   </>
 )
 }
